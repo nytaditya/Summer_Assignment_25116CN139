f36030bd9353f9eb7b4d8fdbab9dfaa6ea93db4b
nytaditya <Adityafr4u@gmail.com>
2026-06-05 01:55:10 +0530

    changes

---
+++ DAY_5/ques_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, digit, i;
+    int fact, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        fact = 1;
+
+        for(i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        n = n / 10;
+    }
+
+    if(sum == temp)
+        printf("Strong Number");
+    else
+        printf("Not a Strong Number");
+
+    return 0;
+}
