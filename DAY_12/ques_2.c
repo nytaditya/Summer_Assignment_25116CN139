c46df4c8869146f5c9b64c5084ecdbfeea3b60b6
nytaditya <Adityafr4u@gmail.com>
2026-06-12 23:23:34 +0530

    day_12

---
+++ DAY_12/ques_2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int armstrong(int n)
+{
+    int sum=0,temp=n,r;
+
+    while(n>0)
+    {
+        r=n%10;
+        sum=sum+r*r*r;
+        n=n/10;
+    }
+
+    if(sum==temp)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number: ");
+    scanf("%d",&n);
+
+    if(armstrong(n))
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+
+    return 0;
+}
